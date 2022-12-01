3d0ad0fe816db82a65d709055ccae3b8e174c653
kennyb39 <chefken360@gmail.com>
2022-12-01 09:41:25 -0800

    seeds sprouting

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-DB_USER=''
-DB_PASSWORD=''
-DB_NAME=library_db
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require("./config/connection");
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
